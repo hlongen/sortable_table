f23441a6a275fb84a8d1bd551a3a95ed38194070
hlongen <hlongen@dkssystems.com>
2018-10-29 12:39:04 -0500

    Update application_helper.rb

--- lib/sortable_table/app/helpers/application_helper.rb
+++ lib/sortable_table/app/helpers/application_helper.rb
@@ -46,7 +46,7 @@ module SortableTable
           end
           
           def sortable_url(opts)
-            url_for(params.merge(:sort => opts[:sort], :order => link_sort_order(opts), :page => 1))
+            url_for(params.merge(:sort => opts[:sort], :order => link_sort_order(opts), :page => 1).permit(:name, :sort, :order))
           end
           
           def link_sort_order(opts)
